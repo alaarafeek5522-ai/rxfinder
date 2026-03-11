d420dcfd1d332ce01961351248df8f2a6a5c34b5
Alaa <alaa@example.com>
2026-03-11 13:09:09 +0200

    fix: add connectivity_plus import to search_screen

--- lib/screens/search_screen.dart
+++ lib/screens/search_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:provider/provider.dart';
 import 'package:flutter_staggered_animations/flutter_staggered_animations.dart';
 import '../providers/medicine_provider.dart';
